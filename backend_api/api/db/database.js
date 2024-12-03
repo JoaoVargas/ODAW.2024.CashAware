4eaef897684c23eec459039fb4ac2f4ae3c26666
João Guilherme Vargas <jotaguivargas@gmail.com>
2024-12-03 01:29:16 -0300

    feito

--- backend_api/api/db/database.js
+++ backend_api/api/db/database.js
@@ -1,9 +1,10 @@
 import { Sequelize } from 'sequelize';
 import defineUsuario from '../models/usuario.js';
+import defineOrcamento from '../models/orcamento.js';
 
 const sequelize = new Sequelize('postgres://postgres:postgres@postgres:5432/cashaware_dev', {dialect: 'postgres'});
 
-defineUsuario(sequelize);
+defineOrcamento(sequelize);
 
 sequelize.sync({ alter: true }).then(console.log('DB is synced'));
 
